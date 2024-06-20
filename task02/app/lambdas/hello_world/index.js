c4633466de41f940d38fbac9657f55d81fe4d858
keelfy <tehnyla@gmail.com>
2024-06-20 15:48:32 +0200

    Fixed function logic

--- task02/app/lambdas/hello_world/index.js
+++ task02/app/lambdas/hello_world/index.js
@@ -1,14 +1,20 @@
 exports.handler = async (event) => {
-    console.log(JSON.stringify(event, null, 2));
+    const path = event.requestContext.http.path;
+    const method = event.requestContext.http.method;
 
-    if (event.httpMethod == 'GET' && event.resource == '/hello') {
-        return {
+    console.log(JSON.stringify(event, null, 2));
+    
+    let response = {
+        statusCode: 400,
+        message: `Bad request syntax or unsupported method. Request path: ${path}. HTTP method: ${method}`
+    };
+
+    if (method === 'GET' && path === '/hello') {
+        response = {
             statusCode: 200,
             message: "Hello from Lambda",
         };
     }
-    return {
-        statusCode: 400,
-        message: `Bad request syntax or unsupported method. Request path: ${event.resource}. HTTP method: ${event.httpMethod}`
-    };
+
+    return response;
 };
